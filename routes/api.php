885ed237c2118c28aa4acb7fdca28fbf54ae562e
savicbranko <savic.bane98@hotmail.com>
2022-09-15 11:41:47 +0200

    user kotroler i izmena koda greske

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ShiftController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('login', [UserController::class, 'login']);
+Route::post('register', [UserController::class, 'register']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('shifts', ShiftController::class);
+    Route::post('logout', [UserController::class, 'logout']);
 });
+
